4ffd20011eb5211b2c373454133d58b1e845918b
Michele Guerreri <52530590+micGuerr@users.noreply.github.com>
2022-07-13 17:46:45 +0200

    Add files via upload

--- doc/solutions/cw04_DL_Fit.m
+++ doc/solutions/cw04_DL_Fit.m
@@ -28,8 +28,6 @@
 
 %% 4.0 Cleaning and loading
 
-%{
-
 % First thing let's clean our workspace, clear the command window and close
 % all open figures:
 
@@ -45,11 +43,7 @@ load(fullfile('coursework_outputs', 'cw1.mat'), 'gccVox')
 load(fullfile('coursework_outputs', 'cw2.mat'), 'noddi', 'acqProtocol')
 load(fullfile('coursework_outputs', 'cw3.mat'), 'gccVox_norm', 'noddiFit_gcc', 'noddiLoglik_gcc');
 
-%}
-
 %% 4.1 Training set
-
-%{
 
 % Remember, the problem of deep learning based parameter estimation is:
 % to learn a function (1) that can predict the parameters of interest (2) 
@@ -73,35 +67,44 @@ load(fullfile('coursework_outputs', 'cw3.mat'), 'gccVox_norm', 'noddiFit_gcc', '
 
 N = 10e4;
 seed1 = 123;
-[modelParam, modelSignal] = generateModelTrainingSet(); % complete this
+[modelParam, modelSignal] = generateModelTrainingSet(N, noddi, acqProtocol, seed1); % complete this
 
 % Let's explore the parameter distribution. Use the plotDist() function to see
 % the generated parameter distributions
 
 % first let's convert the kappa values into odi values
-modelParam_odi = kappa2odi(); % complete this
+modelParam_odi = kappa2odi(modelParam, noddi.name); % complete this
 
 % chech the function kappa2odi to know the relation between the odi and
 % kappa. If kappa is defined [0 +inf] what is the odi range?
-% ANSWER: 
+% ANSWER: ODI ranges between [0 1].
 
-plotDist(); % complete this
+fig_paramDist = plotDist(modelParam_odi , noddi.paramsStr, noddi.name); % complete this
 
 
 % Describe the distributions characterizing each parameter. Do they make
 % sense to you?
 % ANSWER:
-
+% 1. NDI or ficvf is extracted from a uniform distribution between [0 1].
+% 2. di is fixed. 
+% 3. ODI is connected to the parameter kappa.  it is extracted from a 
+%    uniform distribution between [0 1].
+% 4. FWF or fiso is extracted from a bimodal distribution mimicking invivo
+%    distribution
+% 5. d_FWF or diso is fixed.
+% 6. b0 is fixed. We use a value of 1.
+% 7. theta is extracted from a cos distribution between [0 and pi/2].
+% 8. phi is extracted from a uniform distribution between [0 2pi].
 
 % you can give a try with modelParam variable and check what distribution
 % of kappa corresponds to a uniform distribution of odi
 
-plotDist(); % complete this
+plotDist(modelParam , noddi.paramsStr, noddi.name); % complete this
 
 % plot the signal of the first voxel to check that everything is all right. Use b0=1.
 fig_train_compact = figure('Position', [400 100 500 800], 'color', [ 1 1 1]);
-fibredir = GetFibreOrientation(); % complete this
-VoxelDataViewer(); % complete this
+fibredir = GetFibreOrientation(noddi.name, modelParam(1,:)); % complete this
+VoxelDataViewer(acqProtocol, modelSignal(1,:)', fibredir, 1, fig_train_compact); % complete this
 
 % let's now add some noise to the data to make them look as if they were
 % data acquired experimentally.
@@ -114,24 +117,27 @@ snr = 30;
 % again, we set a random seed
 seed2 = 234;
  
-modelSignal_noisy = addNoise2Signal(); % complete this
+modelSignal_noisy = addNoise2Signal(modelSignal, snr, seed2); % complete this
 
 % OPTIONAL: have a look at the addNoise2Signal function. How is noise
 % generated? What type of distribution is that? Any clue why?
-% ANSWER: 
+% ANSWER: the MRI images we usually deal with are obtained as the modulus of
+% a real and imaginary part of the acquired signal. 
+% Both parts have Gaussian noise, and the resulting image has nonlinear 
+% combination of these two noise components.
+% The distribution of this particular random variable is known as Rician
+% distribution.
 
 % Let's plot the first voxel noisy signal
 fig_noisy_compact = figure('Position', [400 100 500 800], 'color', [ 1 1 1]);
-VoxelDataViewer(); % complete this
+VoxelDataViewer(acqProtocol, modelSignal_noisy(1,:)', fibredir, 1, fig_noisy_compact); % complete this
 
 % Compare it with the GCC voxel signal plot. Do they look similar?
 load(fullfile('coursework_outputs', 'cw1.mat'), 'fig_gcc_compact');
 
-%}
+
 
 %% 4.2 DL model
-
-%{
 
 % Once we obtained the training set let's define the DL network, i.e. the
 % function that we will use to predict the parameters given the dMRI
@@ -146,13 +152,17 @@ load(fullfile('coursework_outputs', 'cw1.mat'), 'fig_gcc_compact');
 % (we are not going to estimate phi and theta)
 
 % What is the input of the network? What is the input size?
-% ANSWER: 
+% ANSWER: the input is the dMRI data for each voxel at the time. The size
+% is 81,
 
 % What is the output to the network? What is the output size?
-% ANSWER:
+% ANSWER: the output is the set of parameters we want to estimate for the
+% specific voxel we are analyzing. We focus on 3 parameters: ficvf, kappa
+% (actually odi) and fiso, i.e. the non-fixed  orientation-independent 
+% parameters of NODDI.
 
-numOfFeatures = ; % complete this
-numOfOutputs = ; % 2 are theta and phi 
+numOfFeatures = acqProtocol.totalmeas; % complete this
+numOfOutputs = sum(~noddi.GD.fixed) - 2; % 2 are theta and phi 
 
 % build the network
 layers = buildDL_function(numOfFeatures, numOfOutputs); % complete this
@@ -163,13 +173,18 @@ analyzeNetwork(layers);
 % How many layers are there? What type of layers? How many learnable
 % parameters? Are you able to deduce the number of parameters given the
 % architecture?
-% 
-
-%}
+% ANSWER: There are 9 layers in total.
+% 1. is the input layer, which takes the dMRI data.
+% 2. is the 1st fully connected layer.
+% 3. is the activation layer of the 1st fc layer. Matlab treats these
+% layers separately.
+% 4-7 .are 2 other fc layers with the connected activation function.
+% 8. is the output layer
+% 9. is the regression layer which computes the similarity metric.
+%
+% The learnable parameters are 58000.
 
 %% 4.3 Network training
-
-%{
 
 % the next step is to update the function (network) we just defined so that 
 % we can learn the mapping between input data and output parameters.
@@ -183,8 +198,8 @@ analyzeNetwork(layers);
 hPercentage = 0.2;
 seed3 = 345;
 % the train params are only the three we are willing to estimate
-trainParam = ; % complete this
-[xTrain, yTrain, xValidation, yValidation] =   trainValSplit(); % complete this
+trainParam = modelParam_odi(:, [1 3 4]); % complete this
+[xTrain, yTrain, xValidation, yValidation] =   trainValSplit(modelSignal_noisy, trainParam, N, hPercentage, seed3); % complete this
 
 % Dose the output make sense to you?
 
@@ -203,7 +218,7 @@ mlpOptions = trainingOptions('adam', ...
 % couple of minutes and stop it with the button at the top right corner of
 % the training GUI.
 rng(456);
-net = trainNetwork(); % complete this
+net = trainNetwork(xTrain, yTrain, layers, mlpOptions); % complete this
 
 % I completed the training for you.
 load('..\..\Data\DL_paramEstimation\trained_network.mat', 'net')
@@ -211,11 +226,7 @@ load('..\..\Data\DL_paramEstimation\trained_network.mat', 'net')
 % Have a look at the training progress window. Try to understand what each
 % line means. What the iterations are? What the epochs are?
 
-%}
-
 %% 4.4 Training assessment
-
-%{
 
 % It seems the training is doing something interesting, but how can we
 % judge the quality of the training?
@@ -226,33 +237,40 @@ load('..\..\Data\DL_paramEstimation\trained_network.mat', 'net')
 valPrediction = predict(net, xValidation); % complete this
 
 % Let's compute the overall RMSE...
-val_rmse = ; % complete this
+val_rmse = mean( sqrt( mean((valPrediction-yValidation).^2, 1) ), 2 ); % complete this
 
 % What value do you get?
-% 
+% ANSWER: 0.0848
 
 % how does the validation RMSE looks compared to the one obtained for the
 % synthetic voxel in course work 3? Does it make sense two compare the two?
-% ANSWER: 
+% ANSWER: It doesn't make any sense because the RMSE we were computing
+% before was computed on the experimental and synthesized measurements.
+%  this RMSE is computed on ground truth partameters and estimated
+% parameters.
 
 % Also we can have a look at the plots of ground truth vs predicted value:
 fig_netPredictionQC = figure('Position', [400 100 1300 400], 'color', [ 1 1 1]);
-subplot(1,3,1), plot_gt_vs_prediction(, 'NDI (ficvf)'); % complete this
-subplot(1,3,2), plot_gt_vs_prediction(, 'ODI'); % complete this
-subplot(1,3,3), plot_gt_vs_prediction(, 'FWF (fiso)'); % complete this
+subplot(1,3,1), plot_gt_vs_prediction(yValidation(:,1), valPrediction(:,1), 'NDI (ficvf)'); % complete this
+subplot(1,3,2), plot_gt_vs_prediction(yValidation(:,2), valPrediction(:,2), 'ODI'); % complete this
+subplot(1,3,3), plot_gt_vs_prediction(yValidation(:,3), valPrediction(:,3), 'FWF (fiso)'); % complete this
 
 % How do the plots look?
-% ANSWER: 
+% ANSWER: The data mostly follow the identy line which suggests the estimation
+% is overall good. There is a group of data consistently taking 0.5 for
+% any value of NDI and of ODI. This data probably corresponds to voxels in
+% which FWF is very high, in which is basically impossible assign a value
+% to ODI and NDI (there are no tissues). 
+% In this "undetermined" situations the DL model predict the mean of the distribution ~0.5.
+% You can verify this easely plotting the odi or ndi predicted values vs the FWF gt
 
-figure,plot_gt_vs_prediction(, 'NDI VS FWF'); % complete this
-figure,plot_gt_vs_prediction(, 'ODI VS FWF'); % complete this
-figure,plot_gt_vs_prediction(, 'ODI VS NDI'); % complete this
+figure,plot_gt_vs_prediction(yValidation(:,3), valPrediction(:,1), 'NDI VS FWF'); % complete this
+figure,plot_gt_vs_prediction(yValidation(:,3), valPrediction(:,2), 'ODI VS FWF'); % complete this
+figure,plot_gt_vs_prediction(yValidation(:,1), valPrediction(:,2), 'ODI VS NDI'); % complete this
 
-%}
 
-%% 4.5 Extend to an unseen voxel
 
-%{
+%% Extend to an unseen voxel
 
 % We can now try to extend our analysis to unseen data. We will use the
 % GCC voxel again.
@@ -263,32 +281,35 @@ gccParPrediction = predict(net, gccVox_norm'); % complete this
 
 % This time we don't have the parameter ground truth. How can we assess the
 % quality of the estimation?
-% ANSWER:
+% ANSWER: we can use the parameters of the conventional fit as ground trurh
+% for example and compute the RMSE. Alternatively we can generate a version
+% of the signal from the parameters and use it to compute the
+% log-likelihood.
 
 % Let's use the parameters estimated by the network to estimate the signal.
 % we use the theta and phi from the conventional fit approach.
 noddiNet_gcc_odi = noddiFit_gcc;
-noddiNet_gcc_odi([1 3 4]) = ; % complete this
-noddiNet_gcc = odi2kappa(); % complete this
+noddiNet_gcc_odi([1 3 4]) = gccParPrediction; % complete this
+noddiNet_gcc = odi2kappa(noddiNet_gcc_odi, noddi.name); % complete this
 
 % compute the loglikelihood and get the fit quality plot. Compare both
 % results with those obtained via the conventional fit approach.
-loglik_gccnet = getFitRicianLogLik(); % complete this
-fig_noddiQC_gccnet = QualityOfFit(); % complete this
+loglik_gccnet = getFitRicianLogLik(noddiNet_gcc, acqProtocol, noddi, gccVox); % complete this
+fig_noddiQC_gccnet = QualityOfFit(gccVox, noddiNet_gcc, noddi, acqProtocol); % complete this
 
 load(fullfile('coursework_outputs', 'cw3.mat'), 'fig_noddiQC_gcc');
 
 % ANSWER: 
-%
+% loglik_gccnet=-5.0265e+05 vs noddiLoglik_gcc=-4.5843e+05.
+% The network loglikelihood is higher in absolute value, which means the fit
+% from DL is worse compared to that from conventional fit.
+% Nontheless, the quality fit plot shows that the model from the DL derived
+% parameters describes well the data.
 
 % We showed that our DL network, trained on synthetic data can explain
 % in-vivo data quite well.
 
-%}
-
-%% 4.6 Extend the analysis to in-vivo data
-
-%{
+%% 4.7 Extend the analysis to in-vivo data
 
 % In the last step of our analysis we are going to deploy our DL model on a
 % set of invivo data (which include our GCC voxel).
@@ -304,11 +325,11 @@ fittedParams_path = fullfile('..','..','NODDI_example_dataset','output','FittedP
 load(fittedParams_path, 'mlps', 'fobj_ml')
 
 % you need first to normalize the data
-roi_norm = ; % complete this
+roi_norm = roi./mean(roi(:,acqProtocol.b0_Indices),2); % complete this
 
 % Let's use the network to predict the parameters
 tic
-fittedParams_dl = predict(); % complete this
+fittedParams_dl = predict(net, roi_norm); % complete this
 dl_fitTime = toc;
 
 % how long did it take? We fitted ~5500 voxels. Try to work out the time
@@ -349,19 +370,18 @@ end
 % loglikelihood for each voxel and used that fo comparison...
 % Do it if you have time :D !
 
-%}
 
-%% 4.7 Save the output
 
-%{
+%% 4.6 Save the output
+
+
 
 % Before running this section make sure you have finished your analysis as
 % this will close all the open figures
 
-% let's now save the variables we created into a mat fi
-save(fullfile('coursework_outputs', 'cw4.mat'))
-
 % and close the figures
 close all
 
-%}
+
+% let's now save the variables we created into a mat fi
+save(fullfile('coursework_outputs', 'cw4.mat'), 'net', 'gccParPrediction', 'loglik_gccnet')
